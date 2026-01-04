6fb0c71f3ed35535f787ee093c2b20da711c08bb
erlint1212 <e.t.nordtvedt@gmail.com>
2026-01-04 18:51:25 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RAG training project
---
+++ shell.nix
@@ -0,0 +1,23 @@
+# For nix package manager, dependencies
+let
+  pkgs = import <nixpkgs> { config.allowUnfree = true; };
+in pkgs.mkShell {
+  packages = [
+    pkgs.uv
+  ];
+  shellHook = ''
+    # --- Virtual Environment Setup ---
+    if [ ! -d ".venv" ]; then
+        echo "Creating Python virtual environment (.venv) with UV..."
+        uv init .
+        uv venv
+        source .venv/bin/activate
+        uv add google-genai==1.12.1
+        uv add python-dotenv==1.1.0
+    else
+        source .venv/bin/activate
+    fi
+
+    export PS1="\n\[\033[1;32m\][RAG_Hoopla_env:\w]\$\[\033[0m\]"
+  '';
+}
